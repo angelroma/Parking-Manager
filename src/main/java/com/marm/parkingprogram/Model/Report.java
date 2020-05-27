e9f27e4fedfed0abfef30ec104048960e10322e3
Angel Rodriguez <angelr10mar@gmail.com>
2020-05-27 10:59:28 -0500

    Add MVC arquitecture

---
+++ src/main/java/com/marm/parkingprogram/Model/Report.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.marm.parkingprogram.Model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author anr10
+ */
+@Entity
+@Table(catalog = "parkingcompany", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Report.findAll", query = "SELECT r FROM Report r"),
+    @NamedQuery(name = "Report.findById", query = "SELECT r FROM Report r WHERE r.id = :id"),
+    @NamedQuery(name = "Report.findByTotalTime", query = "SELECT r FROM Report r WHERE r.totalTime = :totalTime"),
+    @NamedQuery(name = "Report.findByTotalPrice", query = "SELECT r FROM Report r WHERE r.totalPrice = :totalPrice")})
+public class Report implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private float totalTime;
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private float totalPrice;
+
+    public Report() {
+    }
+
+    public Report(Integer id) {
+        this.id = id;
+    }
+
+    public Report(Integer id, float totalTime, float totalPrice) {
+        this.id = id;
+        this.totalTime = totalTime;
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public float getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(float totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Report)) {
+            return false;
+        }
+        Report other = (Report) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.marm.parkingprogram.Model.Report[ id=" + id + " ]";
+    }
+    
+}
